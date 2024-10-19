dc5b9c130990e73d4bf8596a69618a206c07f9a3
em429 <1897608+em429@users.noreply.github.com>
2024-10-19 03:11:11 +0200

    chore: update config to use env vars for all values

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import os
 class Config:
     DEFAULT_DB_PATH = os.path.join(os.path.expanduser("~"), "R", "books", "metadata.db")
     DB_PATH = os.getenv("BOOKS_DB_PATH", DEFAULT_DB_PATH)
-    STATE_FILE = "state.json"
+    STATE_FILE = os.getenv("HIGHLIGHTS_STATE_FILE", "state.json")
+    SECRET_KEY = os.getenv("SECRET_KEY", "secret")
